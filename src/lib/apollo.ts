aa777750161b1cac2ebec71420d47803e4069f6a
Miguel Rocha <miguelrocha.dev@gmail.com>
2022-08-07 04:20:58 +0100

    aslnlkas

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,11 +1,11 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const CONTENT_API_DEV_TOKEN = process.env.CONTENT_API_DEV_TOKEN;
+const CONTENT_API_TOKEN = process.env.CONTENT_API_TOKEN;
 
 export const apolloClient = new ApolloClient({
   uri: process.env.CONTENT_API,
   cache: new InMemoryCache(),
   headers: {
-    Authorization: `Bearer ${CONTENT_API_DEV_TOKEN}`,
+    Authorization: `Bearer ${CONTENT_API_TOKEN}`,
   },
 });
